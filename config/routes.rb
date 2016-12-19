a2dd943753f57077923491e023f97fff944bbe80
Faskill <fatskill@hotmail.fr>
2016-12-19 13:45:19 +0000

    Typo fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :use, controllers: {
+  devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
     registrations: 'users/registrations'
