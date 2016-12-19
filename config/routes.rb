8d25ce66876856c8d6779b985a0c3f11899acb01
Faskill <fatskill@hotmail.fr>
2016-12-19 13:29:12 +0000

    Generated devise views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :use, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
   resources :articles
   mount Ckeditor::Engine => '/ckeditor'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
